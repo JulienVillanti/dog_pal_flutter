64f8b3fc94db379f4cc993788eb2f868fad8f0f1
JulienVillanti <57160259+JulienVillanti@users.noreply.github.com>
2024-11-17 18:02:27 -0500

    Created project and splash page

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image.asset(
+            'assets/dogpal-logo.png',
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
