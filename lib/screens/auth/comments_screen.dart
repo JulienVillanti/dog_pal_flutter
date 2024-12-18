13b47e8af44f26c1c79225b474feeb6569fd8e3c
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2024-12-18 11:55:45 -0500

    added empty widgets to implement the code later

---
+++ lib/screens/auth/comments_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CommentsScreen extends StatelessWidget {
+  const CommentsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Comments'),
+      ),
+      body: const Center(
+        child: Text('comments demo'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CommentsScreen(),
+    );
+  }
+}
