6f6e3b5f91ae3f1e36294ac099f09df2b8f2fc26
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2024-12-19 22:38:52 -0500

    profile creation half way done. need to add button.

---
+++ lib/screens/auth/profile_creation_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class UserProfileCreationView extends StatefulWidget {
+  @override
+  _UserProfileCreationViewState createState() => _UserProfileCreationViewState();
+}
+
+class _UserProfileCreationViewState extends State<UserProfileCreationView> {
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _userEmailController = TextEditingController();
+  final TextEditingController _userAgeController = TextEditingController();
+  final TextEditingController _dogNameController = TextEditingController();
+  final TextEditingController _dogBreedController = TextEditingController();
+
+  File? _userImage;
+
+  final ImagePicker _picker = ImagePicker();
+
+  bool _isSubmitting = false;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _userImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _updateUserProfileStatus() async {
+    setState(() {
+      _isSubmitting = true;
+    });
+
+    try {
+      final uid = FirebaseAuth.instance.currentUser?.uid;
+      if (uid == null) throw Exception("User not logged in");
+
+      final userRef = FirebaseDatabase.instance.ref().child('users').child(uid);
+
+      final userData = {
+        "profileCreated": true,
+        "userName": _userNameController.text,
+        "userEmail": _userEmailController.text,
+        "userAge": _userAgeController.text,
+        "dogName": _dogNameController.text,
+        "dogBreed": _dogBreedController.text,
+        "userImage": _userImage != null ? _userImage!.path : null,
+      };
+
+      await userRef.update(userData);
+      Navigator.pushReplacementNamed(context, '/homeScreen');
+    } catch (e) {
+      print("Failed to update profile status: $e");
+    } finally {
+      setState(() {
+        _isSubmitting = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Set up your account'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Logo
+            Image.asset(
+              'assets/DogPalLogo2.png',
+              height: 150,
+            ),
+
+            SizedBox(height: 16.0),
+
+            // Input fields
+            TextField(
+              controller: _userNameController,
+              decoration: InputDecoration(labelText: 'Your Name'),
+            ),
+            TextField(
+              controller: _userEmailController,
+              decoration: InputDecoration(labelText: 'Your Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _userAgeController,
+              decoration: InputDecoration(labelText: 'Your Age'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: _dogNameController,
+              decoration: InputDecoration(labelText: 'Dog Name'),
+            ),
+            TextField(
+              controller: _dogBreedController,
+              decoration: InputDecoration(labelText: 'Dog Breed'),
+            ),
+
+            SizedBox(height: 16.0),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
